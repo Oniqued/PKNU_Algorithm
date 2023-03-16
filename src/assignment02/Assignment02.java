bd46427f7f2557c1ba4013b5482ab51f37854fb4
Oniqued <zktl3575@gmail.com>
2023-03-16 16:03:55 +0900

    장기 말 '포' 문제 - 완료

--- src/assignment02/Assignment02.java
+++ src/assignment02/Assignment02.java
@@ -6,7 +6,10 @@ import java.util.Scanner;
 
 public class Assignment02 {
     static int N, x, y, x_, y_;
+    static int[] dx = {-1, 1, 0, 0}; // 상, 하, 좌, 우
+    static int[] dy = {0, 0, -1, 1};
     static int[][] board;
+    static boolean[][] visited;
 
     public static void main(String[] args) throws IOException {
         readFile();
@@ -24,6 +27,7 @@ public class Assignment02 {
 
         N = sc.nextInt();
         board = new int[N][N];
+        visited = new boolean[N][N];
 
         for(int i = 0; i < N; i++){
             for(int j = 0; j < N; j++){
@@ -38,11 +42,31 @@ public class Assignment02 {
     }
 
     static boolean move(int sx, int sy) {
+//        System.out.printf("(%d, %d)\n", sx, sy);
         //목표지점 도달시 정지
-        if (sx == x_ && sy == x_) {
+        if (sx == x_ && sy == y_) {
             return true;
         }
-        //동쪽에
+        for (int i = 0; i < 4; i++) {
+            int nx = sx;
+            int ny = sy;
+            int cnt = 0; // 현재 방향에 한칸짜리 말이 존재하는가 ?
+            while(true){
+                nx += dx[i];
+                ny += dy[i];
+                if (nx < 0 || nx >= N || ny < 0 || ny >= N) { // 보드를 넘어가지 않는지 체크
+                    break; // while 문 빠져 나가고 다른 방향에 대해서 다시 한칸씩 검사
+                }
+                if (board[nx][ny] == 1) {
+                    cnt++;
+                    continue;
+                }
+                if (cnt == 1 && !visited[nx][ny]) {
+                    visited[nx][ny] = true; //이 cell이 있는 방향으론 되돌아가면 안되므로 방문 표시
+                    if(move(nx, ny)) return true;
+                }
+            }
+        }
         return false;
     }
 }
